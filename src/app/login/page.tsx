6794fb850b974def5aeb4cc8f90b45156962be39
milyasbpa <basilyas.2015@gmail.com>
2025-03-21 13:43:52 +0700

    fix: error build

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,5 @@
-import { AppContainer } from "@/core/modules/app/container";
 import { LoginAuthContainer } from "@/features/auth/login/container";
 import { LoginAuthProvider } from "@/features/auth/login/context";
-import { ChatTripContainer } from "@/features/trip/chat/container";
 
 export default function LoginPage() {
   return (
