5f06e38064d6b6ba91edc3e03dd08fa52074522b
milyasbpa <basilyas.2015@gmail.com>
2025-03-22 11:35:07 +0700

    fix: remove app container

--- src/app/(trip)/page.tsx
+++ src/app/(trip)/page.tsx
@@ -1,4 +1,3 @@
-import { AppContainer } from "@/core/modules/app/container";
 import { FindTripContainer } from "@/features/trip/find/container/Find.trip.container";
 import { FindTripProvider } from "@/features/trip/find/context";
 
