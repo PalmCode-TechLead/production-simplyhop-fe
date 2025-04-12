96358c7f1d6cbf878d1b8e592845d347852d8c0c
milyasbpa <basilyas.2015@gmail.com>
2025-04-12 10:59:55 +0700

    fix: animation in top navbar

--- src/core/modules/app/fragments/top_navigation_mobile_menu/TopNavigationMobileMenu.app.tsx
+++ src/core/modules/app/fragments/top_navigation_mobile_menu/TopNavigationMobileMenu.app.tsx
@@ -6,6 +6,7 @@ import { AppCollectionURL } from "@/core/utils/router/constants/app";
 import Cookies from "universal-cookie";
 import { usePathname } from "next/navigation";
 import SVGIcon, { SVGIconProps } from "@/core/icons";
+import { motion, AnimatePresence } from "framer-motion";
 
 export const TopNavigationMobileMenu = () => {
   const dictionaries = getDictionaries();
@@ -19,9 +20,15 @@ export const TopNavigationMobileMenu = () => {
     setIsOpen((prev) => !prev);
   };
 
-  useEffect(()=>{
-    setIsOpen(false)
-  },[pathname])
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
+  const variants = {
+    hidden: { y: -20, opacity: 0 },
+    visible: { y: 0, opacity: 1 },
+    exit: { y: -20, opacity: 0 },
+  };
   return (
     <>
       <button
@@ -34,70 +41,74 @@ export const TopNavigationMobileMenu = () => {
         />
       </button>
 
-      {isOpen && (
-        <div
-          className={clsx(
-            "grid grid-rows-1 grid-cols-1 items-center content-center justify-end justify-items-end gap-4 lg:gap-8",
-            "w-full",
-            "fixed top-[90px] left-0 right-0",
-            "w-full",
-            "z-30",
-            "bg-[white]",
-            "px-[1rem]"
-          )}
-        >
-          <div
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            key="mobile-menu"
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            variants={variants}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
             className={clsx(
-              "grid grid-cols-1 items-center content-center justify-end justify-items-end gap-4 lg:gap-8",
-              "w-full h-full"
+              "grid grid-rows-1 grid-cols-1 items-center content-center justify-end justify-items-end gap-4 lg:gap-8",
+              "fixed top-[90px] left-0 right-0",
+              "w-full z-30 bg-white px-[1rem]"
             )}
           >
-            {dictionaries.menu.items.map((menu, menuIndex) => (
-              <Link
-                {...menu}
-                href={
-                  menu.id === "mitfahrt-anbieten" && !isLogin
-                    ? AppCollectionURL.public.login()
-                    : menu.id === "support" && !isLogin
-                    ? AppCollectionURL.public.login()
-                    : menu.href
-                }
-                key={menuIndex}
-                className={clsx(
-                  "grid grid-flow-col items-center content-center justify-start justify-items-start gap-[0.5rem]",
-                  "h-[2.5rem]",
-                  "w-full",
-
-                  pathname === menu.href && menu.id === "mitfahrt-anbieten"
-                    ? "text-[#333FFF]"
-                    : pathname === menu.href
-                    ? "text-green-500"
-                    : pathname.includes(menu.id)
-                    ? "text-green-500"
-                    : "text-neutral-300",
-                  "text-[1rem] font-semibold text-inter"
-                )}
-              >
-                <SVGIcon
-                  {...(menu.icon as { name: SVGIconProps["name"] })}
-                  key={`svgIcon.${menuIndex}`}
+            <div
+              className={clsx(
+                "grid grid-cols-1 items-center content-center justify-end justify-items-end gap-4 lg:gap-8",
+                "w-full h-full"
+              )}
+            >
+              {dictionaries.menu.items.map((menu, menuIndex) => (
+                <Link
+                  {...menu}
+                  href={
+                    menu.id === "mitfahrt-anbieten" && !isLogin
+                      ? AppCollectionURL.public.login()
+                      : menu.id === "support" && !isLogin
+                      ? AppCollectionURL.public.login()
+                      : menu.href
+                  }
+                  key={menuIndex}
                   className={clsx(
-                    "w-[1rem] h-[1rem]",
+                    "grid grid-flow-col items-center content-center justify-start justify-items-start gap-[0.5rem]",
+                    "h-[2.5rem]",
+                    "w-full",
+
                     pathname === menu.href && menu.id === "mitfahrt-anbieten"
                       ? "text-[#333FFF]"
                       : pathname === menu.href
                       ? "text-green-500"
                       : pathname.includes(menu.id)
                       ? "text-green-500"
-                      : "text-neutral-300"
+                      : "text-neutral-300",
+                    "text-[1rem] font-semibold text-inter"
                   )}
-                />
-                {menu.name}
-              </Link>
-            ))}
-          </div>
-        </div>
-      )}
+                >
+                  <SVGIcon
+                    {...(menu.icon as { name: SVGIconProps["name"] })}
+                    key={`svgIcon.${menuIndex}`}
+                    className={clsx(
+                      "w-[1rem] h-[1rem]",
+                      pathname === menu.href && menu.id === "mitfahrt-anbieten"
+                        ? "text-[#333FFF]"
+                        : pathname === menu.href
+                        ? "text-green-500"
+                        : pathname.includes(menu.id)
+                        ? "text-green-500"
+                        : "text-neutral-300"
+                    )}
+                  />
+                  {menu.name}
+                </Link>
+              ))}
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
