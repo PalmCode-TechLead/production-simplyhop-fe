8c62e99df75d7d0461ec5791098e5ff3bfcbf8a2
milyasbpa <basilyas.2015@gmail.com>
2025-04-12 10:54:13 +0700

    fix: bottom sheet requirement

--- src/core/components/full_bottom_sheet/FullBottomSheet.tsx
+++ src/core/components/full_bottom_sheet/FullBottomSheet.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import clsx from "clsx";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 export interface FullBottomSheetProps {
   isOpen?: boolean;
@@ -11,21 +11,22 @@ export const FullBottomSheet = ({
   isOpen = false,
   children,
 }: FullBottomSheetProps) => {
-  if (!isOpen) {
-    return null;
-  }
   return (
-    <motion.div
-      className={clsx(
-        "fixed top-0 bottom-0 left-0 right-0",
-        "h-[100vh] bg-[rgba(255,255,255)] z-[1000]"
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          className={clsx(
+            "fixed top-0 bottom-0 left-0 right-0",
+            "h-[100vh] bg-[rgba(255,255,255)] z-[1000]"
+          )}
+          initial={{ y: "100%" }}
+          animate={{ y: 0 }}
+          exit={{ y: "100%" }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+        >
+          {children}
+        </motion.div>
       )}
-      initial={{ y: "100%" }}
-      animate={{ y: 0 }}
-      exit={{ y: "100%" }}
-      transition={{ duration: 0.5, ease: "easeInOut" }}
-    >
-      {children}
-    </motion.div>
+    </AnimatePresence>
   );
 };
