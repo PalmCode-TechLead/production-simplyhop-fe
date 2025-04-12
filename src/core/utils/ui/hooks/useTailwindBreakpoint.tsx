e0dac2ad42c5e0f2a37369ff68a458ca36df80b5
milyasbpa <basilyas.2015@gmail.com>
2025-04-12 10:38:57 +0700

    fix: tailwind breakpoint

--- src/core/utils/ui/hooks/useTailwindBreakpoint.tsx
+++ src/core/utils/ui/hooks/useTailwindBreakpoint.tsx
@@ -3,10 +3,10 @@ import { useMediaQuery } from "usehooks-ts";
 
 export const useTailwindBreakpoint = () => {
   const isXs = useMediaQuery("(max-width: 639px)");
-  const isSm = useMediaQuery("(min-width: 640px) and (max-width: 767px)");
-  const isMd = useMediaQuery("(min-width: 768px) and (max-width: 1023px)");
-  const isLg = useMediaQuery("(min-width: 1024px) and (max-width: 1279px)");
-  const isXl = useMediaQuery("(min-width: 1280px) and (max-width: 1535px)");
+  const isSm = useMediaQuery("(min-width: 640px)");
+  const isMd = useMediaQuery("(min-width: 768px)");
+  const isLg = useMediaQuery("(min-width: 1024px)");
+  const isXl = useMediaQuery("(min-width: 1280px)");
   const is2xl = useMediaQuery("(min-width: 1536px)");
 
   let current: "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | null = null;
