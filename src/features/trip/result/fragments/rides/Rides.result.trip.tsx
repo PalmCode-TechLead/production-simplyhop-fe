2bd32a39c6b67c82dfc90996d2f2952f75d63b9a
milyasbpa <basilyas.2015@gmail.com>
2025-04-22 23:31:54 +0700

    fix: rides loader

--- src/features/trip/result/fragments/rides/Rides.result.trip.tsx
+++ src/features/trip/result/fragments/rides/Rides.result.trip.tsx
@@ -1,12 +1,44 @@
+'use client'
 import * as React from "react";
 import clsx from "clsx";
 import { RideCardResultTrip } from "../../components/ride_card";
 import { useGetRideSearch } from "../../react_query/hooks";
 import { ResultTripContext } from "../../context";
+import { ListLoader } from "@/core/components/list_loader";
+import { getDictionaries } from "../../i18n";
+import { ListErrorItem } from "@/core/components/list_error_item";
 
 export const RidesResultTrip = () => {
+  const dictionaries = getDictionaries()
   const { state } = React.useContext(ResultTripContext);
-  useGetRideSearch();
+  const { isFetching: isFetchingGetRideSearch } = useGetRideSearch();
+  const isLoading = isFetchingGetRideSearch;
+  
+  if (isLoading) {
+    return (
+      <div
+        className={clsx(
+          "grid grid-rows-1 grid-cols-1 place-content-center place-items-center gap-[1rem]",
+          "w-full h-[400px]"
+        )}
+      >
+        <ListLoader message={dictionaries.list.loading.message} />
+      </div>
+    );
+  }
+
+  if (!state.rides.data.length) {
+    return (
+      <div
+        className={clsx(
+          "grid grid-rows-1 grid-cols-1 place-content-center place-items-center gap-[1rem]",
+          "w-full h-[400px]"
+        )}
+      >
+        <ListErrorItem message={dictionaries.list.empty_data.message} />
+      </div>
+    );
+  }
 
   return (
     <div
