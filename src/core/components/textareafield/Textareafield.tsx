a67119d08275845c40f23608a4cefc14da571bfc
milyasbpa <basilyas.2015@gmail.com>
2025-04-22 22:33:18 +0700

    fix: text area

--- src/core/components/textareafield/Textareafield.tsx
+++ src/core/components/textareafield/Textareafield.tsx
@@ -31,7 +31,7 @@ export const Textareafield = ({
       <TextareaLabel
         {...labelProps}
         className={clsx(
-          !!value.length
+          !!value.length || !!inputProps?.value
             ? "top-[15%] left-[0.75rem] sm:left-[1.625rem] translate-y-[-50%] text-[0.75rem]"
             : "top-[25%] left-[0.75rem] sm:left-[1.625rem] translate-y-[-50%] text-[0.875rem]",
           "peer-focus:top-[15%] peer-focus:text-[0.75rem] !text-[#5B5B5B] text-[0.75rem]"
