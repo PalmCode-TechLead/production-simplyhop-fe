5aaa9c2c0b31db3c93ab5c7e5ea07be95b9bb225
milyasbpa <basilyas.2015@gmail.com>
2025-04-29 11:57:10 +0700

    fix: find trip

--- src/features/trip/find/container/Find.trip.container.tsx
+++ src/features/trip/find/container/Find.trip.container.tsx
@@ -8,34 +8,24 @@ export const FindTripContainer = () => {
   return (
     <div className={clsx("w-full h-full", "relative")}>
       <MapFindTrip />
+
       <div
         className={clsx(
-          "grid grid-rows-1 grid-cols-1 items-stretch content-between justify-center justify-items-center",
-          "absolute z-[10]",
-          "w-full top-[0rem] h-[calc(100vh-90px)]",
-          "pt-[1.5rem] pb-[0rem] lg:pb-[1.5rem]"
+          "absolute top-[1.5rem] left-[0px] translate-x-[1.5rem] lg:translate-x-[calc(1*(100vw-1344px)/2)] z-[10]",
+          "rounded-[0.5rem] lg:rounded-[1.25rem]",
+          "bg-[blue]"
         )}
       >
-        <div
-          className={clsx(
-            "grid grid-rows-1 items-stretch content-between justify-start justify-items-start",
-            "max-w-container",
-            "w-full h-full"
-          )}
-        >
-          <div
-            className={clsx(
-              "flex flex-col lg:flex-row items-start justify-between",
-              "w-full h-full",
-              "px-[1rem] lg:px-[0rem] pb-[1rem] lg:pb-[0rem]",
-              "max-w-container"
-            )}
-          >
-            <BadgeFindTrip />
-          </div>
+        <BadgeFindTrip />
+      </div>
 
-          <FilterFindTrip />
-        </div>
+      <div
+        className={clsx(
+          "absolute bottom-[0px] lg:bottom-[1.5rem] left-[50%] translate-x-[-50%] z-[10]",
+          "rounded-[0.5rem] lg:rounded-[1.25rem]"
+        )}
+      >
+        <FilterFindTrip />
       </div>
     </div>
   );
