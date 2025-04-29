53b066105e0d0e73c7ea1e28231c5edbde67bdcf
milyasbpa <basilyas.2015@gmail.com>
2025-04-29 11:19:55 +0700

    fix: hover on tab button

--- src/core/components/tab_button/TabButton.tsx
+++ src/core/components/tab_button/TabButton.tsx
@@ -17,13 +17,13 @@ export const TabButton = forwardRef<
         className={clsx(
           "grid grid-flow-col items-center content-center justify-start justify-items-start gap-[1rem]",
           "w-full",
-          selected ? "bg-[#5AC53D0A]" : "bg-[transparent]",
+          selected ? "bg-[#5AC53D0A]" : "bg-[transparent] hover:bg-[#5AC53D0A]",
           selected
             ? "text-[#232323] text-[1rem] font-semibold"
-            : "text-[#828282] text-[1rem] font-normal",
+            : "text-[#828282] text-[1rem] font-normal hover:text-[#232323] hover:text-[1rem] hover:text-semibold",
           selected
             ? "border-b border-b-[#5AC53D]"
-            : "border-b border-b-[white]",
+            : "border-b border-b-[white] hover:border-b hover:border-b-[white]",
           "px-[1rem] py-[0.75rem]",
           "cursor-pointer",
           "whitespace-nowrap",
@@ -42,12 +42,14 @@ export const TabButton = forwardRef<
       className={clsx(
         "grid grid-flow-col items-center content-center justify-start justify-items-start gap-[1rem]",
         "w-full",
-        selected ? "bg-[#5AC53D0A]" : "bg-[transparent]",
+        selected ? "bg-[#5AC53D0A]" : "bg-[transparent] hover:bg-[#5AC53D0A]",
         "rounded-tr-[0.625rem] rounded-br-[0.625rem]",
         selected
           ? "text-[#232323] text-[1rem] font-semibold"
-          : "text-[#828282] text-[1rem] font-normal",
-        selected ? "border-l border-l-[#5AC53D]" : "border-l border-l-[white]",
+          : "text-[#828282] text-[1rem] font-normal hover:text-[#232323] hover:text-[1rem] hover:font-semibold",
+        selected
+          ? "border-l border-l-[#5AC53D]"
+          : "border-l border-l-[white] hover:border-l hover:border-l-[#5AC53D]",
         "px-[1rem] py-[0.75rem]",
         "cursor-pointer",
         "outline-none",
