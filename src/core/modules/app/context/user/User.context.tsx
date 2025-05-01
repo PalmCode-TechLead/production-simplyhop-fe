02d33159fc0d1a012e7e922de71ff1a090b3efa2
milyasbpa <basilyas.2015@gmail.com>
2025-05-01 07:55:09 +0700

    fix: remove refresh user

--- src/core/modules/app/context/user/User.context.tsx
+++ src/core/modules/app/context/user/User.context.tsx
@@ -5,7 +5,6 @@ import React, {
   Dispatch,
   useEffect,
   useRef,
-  useCallback,
 } from "react";
 import {
   UserActionEnum,
@@ -14,8 +13,6 @@ import {
   UserProfile,
 } from "./User.types";
 import { UserProfileReducers } from "./User.reducers";
-import { fetchGetUserProfileData } from "@/core/services/rest/simplyhop/user_profile";
-import { GetUserProfileDataSuccessResponseInterface } from "@/core/models/rest/simplyhop/user_profile";
 
 const initialState: UserInitialStateType = {
   profile: null,
@@ -24,11 +21,9 @@ const initialState: UserInitialStateType = {
 const UserContext = createContext<{
   state: UserInitialStateType;
   dispatch: Dispatch<UserActions>;
-  refreshProfile: () => Promise<void>;
 }>({
   state: initialState,
   dispatch: () => null,
-  refreshProfile: async () => {},
 });
 
 const mainReducer = (
@@ -60,33 +55,8 @@ const UserProvider = (props: {
     }
   }, []);
 
-  const refreshProfile = useCallback(async () => {
-    try {
-      const res = await fetchGetUserProfileData();
-      const user = res as GetUserProfileDataSuccessResponseInterface;
-      dispatch({
-        type: UserActionEnum.SetProfileData,
-        payload: {
-          id: user.data.id,
-          first_name: user.data?.first_name ?? "",
-          last_name: user.data?.last_name ?? "",
-          avatar: user.data.avatar,
-          email: user.data.email,
-          phonenumber: user.data?.mobile ?? "",
-          city: user.data?.city ?? "",
-          about_me: user.data?.profile?.bio ?? "",
-          is_driver: user.data?.is_driver === 1 ? true : false,
-          gender: user.data?.gender ?? null,
-          is_able_to_ride: user.data.can_share_ride,
-        },
-      });
-    } catch (e) {
-      console.error("Failed to refresh profile:", e);
-    }
-  }, []);
-
   return (
-    <UserContext.Provider value={{ state, dispatch, refreshProfile }}>
+    <UserContext.Provider value={{ state, dispatch }}>
       {props.children}
     </UserContext.Provider>
   );
