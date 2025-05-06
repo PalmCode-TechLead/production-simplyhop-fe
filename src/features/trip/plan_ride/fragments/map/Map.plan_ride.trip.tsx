ab085d40f87caaf3296cd0250e461ac71184fd47
milyasbpa <basilyas.2015@gmail.com>
2025-05-06 16:03:26 +0700

    fix: plan ride

--- src/features/trip/plan_ride/fragments/map/Map.plan_ride.trip.tsx
+++ src/features/trip/plan_ride/fragments/map/Map.plan_ride.trip.tsx
@@ -136,7 +136,6 @@ export const MapPlanRideTrip = () => {
         streetViewControl: false,
         cameraControl: false,
         fullscreenControl: false,
-        zoom: !state.map.polyline_path.length ? 12 : undefined,
       }}
     >
       {/* Start Marker */}
