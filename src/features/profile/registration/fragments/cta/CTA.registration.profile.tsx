36d49580124c4b84ba81fd56f4e6386fee9a0006
milyasbpa <basilyas.2015@gmail.com>
2025-05-06 13:31:49 +0700

    fix: user profile

--- src/features/profile/registration/fragments/cta/CTA.registration.profile.tsx
+++ src/features/profile/registration/fragments/cta/CTA.registration.profile.tsx
@@ -11,10 +11,13 @@ import {
   usePostVehicleCreateMy,
 } from "../../react_query/hooks";
 import { MoonLoader } from "@/core/components/moon_loader";
+import { UserActionEnum, UserContext } from "@/core/modules/app/context";
 
 export const CTARegistrationProfile = () => {
   const dictionaries = getDictionaries();
   const { state, dispatch } = React.useContext(RegistrationProfileContext);
+  const { state: userState, dispatch: dispatchUser } =
+    React.useContext(UserContext);
   const {
     mutateAsync: postUserProfileCreate,
     isPending: isPendingPostUserProfileCreate,
@@ -28,6 +31,21 @@ export const CTARegistrationProfile = () => {
 
     if (state.ride_plan.form.offer_trip.selected?.id === "yes") {
       await postVehicleCreateMy();
+    }
+
+    if (userState.profile) {
+      dispatchUser({
+        type: UserActionEnum.SetProfileData,
+        payload: {
+          ...userState.profile,
+          first_name: state.personal_information.form.first_name.value,
+          last_name: state.personal_information.form.last_name.value,
+          city: state.personal_information.form.city.value,
+          phonenumber: state.personal_information.form.phonenumber.value,
+          about_me: state.personal_information.form.about_me.value,
+          gender: state.personal_information.form.gender.selected?.id ?? null,
+        },
+      });
     }
 
     dispatch({
