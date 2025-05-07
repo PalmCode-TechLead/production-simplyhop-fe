082db93c646ea27a5b3f5c76cbf6d99d996551db
milyasbpa <basilyas.2015@gmail.com>
2025-05-08 00:57:50 +0700

    fix: fahrerin label

--- src/core/utils/driver/functions/formatDriverLabel.tsx
+++ src/core/utils/driver/functions/formatDriverLabel.tsx
@@ -2,14 +2,14 @@ export const formatDriverLabel = (
   data: { id: string; name: string }[],
   gender?: string | null
 ) => {
-  if (!gender || gender === "unspecified") {
-    return [];
+  if (gender === "female") {
+    return [
+      {
+        id: data.find((item) => item.id === gender)?.id ?? "",
+        label: "Fahrerin (W)",
+        variant: "danger" as "success" | "danger",
+      },
+    ];
   }
-  return [
-    {
-      id: data.find((item) => item.id === gender)?.id ?? "",
-      label: data.find((item) => item.id === gender)?.name ?? "",
-      variant: "danger" as "success" | "danger",
-    },
-  ];
+  return [];
 };
