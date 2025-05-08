f8175307b5b7c10359973765bec485dc507a1a81
milyasbpa <basilyas.2015@gmail.com>
2025-05-08 18:03:13 +0700

    fix: tooltip

--- src/core/components/departure_item/DepartureItem.tsx
+++ src/core/components/departure_item/DepartureItem.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import clsx from "clsx";
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import SVGIcon from "@/core/icons";
 
 export interface DepartureItemProps {
   place?: string;
@@ -17,14 +19,52 @@ export const DepartureItem = ({
         "w-full"
       )}
     >
-      <p
+      <div
         className={clsx(
-          "text-[0.75rem] font-normal text-[#8C8D89] lg:truncate lg:text-ellipsis max-lg:line-clamp-2",
+          "grid grid-flow-col items-center content-center justify-start justify-items-start gap-[0.25rem]",
           "w-full"
         )}
       >
-        {place}
-      </p>
+        <p
+          className={clsx(
+            "text-[0.75rem] font-normal text-[#8C8D89] lg:truncate lg:text-ellipsis max-lg:line-clamp-2",
+            "w-full"
+          )}
+        >
+          {place}
+        </p>
+        <div
+          className={clsx(
+            "flex items-center justify-center",
+            "w-[1rem] h-[1rem]"
+          )}
+        >
+          <SVGIcon
+            data-tooltip-id={place}
+            name="Info"
+            className={clsx(
+              "w-[0.75rem] h-[0.75rem]",
+              "stroke-[#667085]",
+              "inline-block"
+            )}
+          />
+          <ReactTooltip
+            id={place}
+            place="bottom"
+            variant="info"
+            className={clsx(
+              "!bg-[white] !shadow-lg",
+              "!text-[#212121] !text-[0.75rem] !font-normal",
+              "!max-w-[250px]",
+              "!px-[0.75rem] !py-[0.5rem]",
+              "!rounded",
+              "!opacity-100"
+            )}
+            content={place}
+          />
+        </div>
+      </div>
+
       <p className={clsx("text-[0.875rem] font-semibold text-[black]")}>
         {time}
       </p>
