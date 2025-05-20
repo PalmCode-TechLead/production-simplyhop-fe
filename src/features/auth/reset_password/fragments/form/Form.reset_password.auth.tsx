3f545f346901af23756092a7601e4ff4d6df60ed
milyasbpa <basilyas.2015@gmail.com>
2025-05-20 12:04:10 +0700

    fix: forgot password

--- src/features/auth/reset_password/fragments/form/Form.reset_password.auth.tsx
+++ src/features/auth/reset_password/fragments/form/Form.reset_password.auth.tsx
@@ -13,13 +13,17 @@ import { getError } from "@/core/utils/form";
 import { MoonLoader } from "@/core/components/moon_loader";
 import { usePostAuthResetPassword } from "../../react_query/hooks";
 import { Passwordfield } from "@/core/components/passwordfield";
+import SVGIcon from "@/core/icons";
+import { useRouter } from "next/navigation";
+import { AppCollectionURL } from "@/core/utils/router/constants";
 
 export const FormResetPasswordAuth = () => {
+  const router = useRouter();
   const dictionaries = getDictionaries();
   const globalDictionaries = getGlobalDictionaries();
   const { state, dispatch } = React.useContext(ResetPasswordAuthContext);
   const {
-    mutate: postAuthResetPassword,
+    mutateAsync: postAuthResetPassword,
     isPending: isPendingPostAuthResetPassword,
   } = usePostAuthResetPassword();
 
@@ -73,7 +77,14 @@ export const FormResetPasswordAuth = () => {
   };
 
   const handleClickResetPassword = async () => {
-    postAuthResetPassword();
+    await postAuthResetPassword();
+    dispatch({
+      type: ResetPasswordAuthActionEnum.SetStateData,
+      payload: {
+        ...state.state,
+        step: "success",
+      },
+    });
   };
 
   const isSubmitLoading = isPendingPostAuthResetPassword;
@@ -90,10 +101,14 @@ export const FormResetPasswordAuth = () => {
     isPasswordConfirmationHasNoLength ||
     isPasswordConfirmationInvalid;
 
+  const handleClickClose = () => {
+    router.push(AppCollectionURL.public.login());
+  };
+
   return (
     <div
       className={clsx(
-        "grid grid-cols-1 place-content-start place-items-start gap-[2rem]",
+        "grid grid-cols-1 items-stretch content-between justify-start justify-items-start gap-[2rem]",
         "bg-[white]",
         "px-[1rem] lg:px-[3rem] py-[3rem]",
         "rounded-[1.25rem]",
@@ -104,57 +119,87 @@ export const FormResetPasswordAuth = () => {
         boxShadow: "0px 0px 25px 0px #969C9640",
       }}
     >
-      <div className="w-[148px] h-[40px] flex items-center justify-center">
-        <Image
-          {...dictionaries.form.header.logo}
-          alt={dictionaries.form.header.logo.alt}
-          className="w-[170px] h-[170px] object-contain"
-        />
-      </div>
-
-      <h1 className={clsx("text-[#292929] text-[1.5rem] font-bold")}>
-        {dictionaries.form.title}
-      </h1>
-
       <div
         className={clsx(
-          "grid grid-cols-1 place-content-start place-items-start gap-[1.5rem]",
+          "grid grid-cols-1 place-content-start place-items-start gap-[2rem]",
           "w-full"
         )}
       >
-        <Passwordfield
-          labelProps={{
-            ...dictionaries.form.input.password.labelProps,
-          }}
-          inputProps={{
-            ...dictionaries.form.input.password.inputProps,
-            value: state.form.password.value,
-            onChange: handleChangePassword,
-          }}
-          error={state.form.password.error?.name}
-        />
-        <Passwordfield
-          labelProps={{
-            ...dictionaries.form.input.password_confirmation.labelProps,
-          }}
-          inputProps={{
-            ...dictionaries.form.input.password_confirmation.inputProps,
-            value: state.form.password_confirmation.value,
-            onChange: handleChangePasswordConfirmation,
-          }}
-          error={state.form.password_confirmation.error?.name}
-        />
-
-        <Button
-          className={clsx("px-[1rem] py-[0.75rem]")}
-          disabled={isSubmitDisabled}
-          isLoading={isSubmitLoading}
-          onClick={handleClickResetPassword}
+        <div className="w-[148px] h-[40px] flex items-center justify-center">
+          <Image
+            {...dictionaries.form.header.logo}
+            alt={dictionaries.form.header.logo.alt}
+            className="w-[170px] h-[170px] object-contain"
+          />
+        </div>
+        <div
+          className={clsx(
+            "grid grid-cols-1 place-content-start place-items-start gap-[1rem]",
+            "w-full"
+          )}
         >
-          {isSubmitLoading && <MoonLoader size={20} color={"white"} />}
-          {dictionaries.form.cta.reset_password.children}
-        </Button>
+          <div
+            className={clsx(
+              "grid grid-flow-col items-center content-center justify-start justify-items-start gap-[1rem]",
+              "w-full"
+            )}
+          >
+            <button onClick={handleClickClose}>
+              <SVGIcon
+                name="X"
+                className={clsx("w-[1.5rem] h-[1.5rem]", "text-[#767676]")}
+              />
+            </button>
+            <h1 className={clsx("text-[#292929] text-[1.5rem] font-bold")}>
+              {dictionaries.form.title}
+            </h1>
+          </div>
+
+          <p className={clsx("text-[#888888] text-[0.875rem] font-normal")}>
+            {dictionaries.form.description}
+          </p>
+        </div>
+
+        <div
+          className={clsx(
+            "grid grid-cols-1 place-content-start place-items-start gap-[1.5rem]",
+            "w-full"
+          )}
+        >
+          <Passwordfield
+            labelProps={{
+              ...dictionaries.form.input.password.labelProps,
+            }}
+            inputProps={{
+              ...dictionaries.form.input.password.inputProps,
+              value: state.form.password.value,
+              onChange: handleChangePassword,
+            }}
+            error={state.form.password.error?.name}
+          />
+          <Passwordfield
+            labelProps={{
+              ...dictionaries.form.input.password_confirmation.labelProps,
+            }}
+            inputProps={{
+              ...dictionaries.form.input.password_confirmation.inputProps,
+              value: state.form.password_confirmation.value,
+              onChange: handleChangePasswordConfirmation,
+            }}
+            error={state.form.password_confirmation.error?.name}
+          />
+        </div>
       </div>
+
+      <Button
+        className={clsx("px-[1rem] py-[0.75rem]")}
+        disabled={isSubmitDisabled}
+        isLoading={isSubmitLoading}
+        onClick={handleClickResetPassword}
+      >
+        {isSubmitLoading && <MoonLoader size={20} color={"white"} />}
+        {dictionaries.form.cta.reset_password.children}
+      </Button>
     </div>
   );
 };
